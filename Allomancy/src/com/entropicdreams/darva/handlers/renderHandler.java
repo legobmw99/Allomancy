dea88aeabb9e0db75059594fcae00527a06ac74e
Darva <zendarva@gmail.com>
2013-12-13 16:21:28 -0600

    15 Minute Commit

--- Allomancy/src/com/entropicdreams/darva/handlers/renderHandler.java
+++ Allomancy/src/com/entropicdreams/darva/handlers/renderHandler.java
@@ -2,11 +2,18 @@ package com.entropicdreams.darva.handlers;
 
 import java.util.EnumSet;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
 
 public class renderHandler implements ITickHandler {
+private final Minecraft mc;
 
+	public renderHandler()
+	{
+		mc = Minecraft.getMinecraft();
+	}
 	@Override
 	public void tickStart(EnumSet<TickType> type, Object... tickData) {
 
@@ -14,6 +21,10 @@ public class renderHandler implements ITickHandler {
 
 	@Override
 	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+		
+		ScaledResolution res = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
+		mc.entityRenderer.setupOverlayRendering();
+		
 
 	}
 
