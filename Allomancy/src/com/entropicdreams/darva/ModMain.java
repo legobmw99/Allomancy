6f76da296542d00fbfab44363fa2d1cae2a6a943
Darva <zendarva@gmail.com>
2013-12-13 00:00:54 -0600

    Auto commiter still not working. *sigh* Quick break to roll my own.

--- Allomancy/src/com/entropicdreams/darva/ModMain.java
+++ Allomancy/src/com/entropicdreams/darva/ModMain.java
@@ -1,5 +1,7 @@
 package com.entropicdreams.darva;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,12 +14,14 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-
+ 
 
 @Mod(modid ="allomancyMod", name = "Allomancy", version = "0.0.1" )
 public class ModMain {
 
 	private Item itemGrinder;
+	
+	private Block oreTin;
 	
 	
 	@Instance(value = "allomancyMod")
@@ -30,17 +34,30 @@ public class ModMain {
     public void preInit(FMLPreInitializationEvent event) {
 		
 		initItems();
+		initBlocks();
 	}
 	
 	@EventHandler 
     public void load(FMLInitializationEvent event) {
 		setupItems();
+		setupBlocks();
 	}
 	 @EventHandler
 	    public void postInit(FMLPostInitializationEvent event) {
 		 
 	 }
 	 
+	 
+	 private void initBlocks()
+	 {
+		 oreTin = new Block(501, Material.rock).setHardness(.5f).setStepSound(Block.soundStoneFootstep)
+				 .setCreativeTab(CreativeTabs.tabBlock).setTextureName("allomancy:tinore");
+	 }
+	 private void setupBlocks()
+	 {
+		 GameRegistry.registerBlock(oreTin, "allomancy:oreTin");
+		 LanguageRegistry.addName(oreTin, "Tin Ore");
+	 }
 	 
 	 private void initItems()
 	 {
