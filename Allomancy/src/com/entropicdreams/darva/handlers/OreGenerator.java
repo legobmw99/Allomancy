91b50eedd7ee8119ae8b8f78e7f196e5395d0e4a
Darva <zendarva@gmail.com>
2013-12-13 12:06:00 -0600

    15 Minute Commit

---
+++ Allomancy/src/com/entropicdreams/darva/handlers/OreGenerator.java
@@ -0,0 +1,18 @@
+package com.entropicdreams.darva.handlers;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class OreGenerator implements IWorldGenerator {
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
