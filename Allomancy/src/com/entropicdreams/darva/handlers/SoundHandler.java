b8036eedeb9c21004a6e5b8047cf8eeba17e30b6
Darva <zendarva@gmail.com>
2013-12-14 20:01:57 -0600

    15 Minute Commit

--- Allomancy/src/com/entropicdreams/darva/handlers/SoundHandler.java
+++ Allomancy/src/com/entropicdreams/darva/handlers/SoundHandler.java
@@ -1,7 +1,14 @@
 package com.entropicdreams.darva.handlers;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.client.particle.EntityFlameFX;
+import net.minecraftforge.client.event.sound.PlaySoundEffectEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.client.event.sound.PlaySoundSourceEvent;
+import net.minecraftforge.client.event.sound.SoundResultEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 
 public class SoundHandler {
@@ -9,7 +16,19 @@ public class SoundHandler {
 	@ForgeSubscribe
 	public void onSound(PlaySoundEvent event)
 	{
-		System.out.println(event.name);
+		EntityClientPlayerMP player;
+		player = Minecraft.getMinecraft().thePlayer;
+		switch (event.name)
+		{
+		case "step.stone":
+		case "step.grass":
+			EntityFX particle = new EntityFlameFX(null, player.posX, player.posY, player.posZ, 0, 0, 0);
+			Minecraft.getMinecraft().effectRenderer.addEffect(particle);
+		break;
+		default:
+			System.out.println(event.name);
+		
+		}
 		
 	}
 }
