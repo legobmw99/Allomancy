46dfb054773da85a4050bb9479ca6c4d4dc32b02
Brian <legobmw99@gmail.com>
2017-01-02 16:02:36 -0500

    Change power command to accept player
    Far more versatile

--- src/main/java/common/legobmw99/allomancy/util/PowerCommand.java
+++ src/main/java/common/legobmw99/allomancy/util/PowerCommand.java
@@ -1,6 +1,7 @@
 package common.legobmw99.allomancy.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import common.legobmw99.allomancy.common.AllomancyCapabilities;
@@ -10,6 +11,7 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
@@ -21,15 +23,11 @@ public class PowerCommand extends CommandBase {
 
     private final List aliases;
     protected int level;
-    protected String[] names = {"Uninvested","Iron Misting","Steel Misting","Tin Misting","Pewter Misting","Zinc Misting","Brass Misting","Copper Misting","Bronze Misting","Mistborn"};
+    protected String[] names = { "Uninvested", "Iron Misting", "Steel Misting", "Tin Misting", "Pewter Misting", "Zinc Misting", "Brass Misting", "Copper Misting", "Bronze Misting", "Mistborn" };
+
     public PowerCommand() {
         aliases = new ArrayList();
         aliases.add("ap");
-    }
-
-    @Override
-    public int compareTo(ICommand arg0) {
-        return 0;
     }
 
     @Override
@@ -39,7 +37,7 @@ public class PowerCommand extends CommandBase {
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return "Usage: /allomancy_power <number> Number is the player's allomancy power, 0 through 9";
+        return "/allomancy_power <number> [player] \nNumber is the player's allomancy power, 0 through 9";
     }
 
     @Override
@@ -53,30 +51,33 @@ public class PowerCommand extends CommandBase {
         World world = sender.getEntityWorld();
 
         if (world.isRemote) {
-            
+
         } else {
-            if ((args.length == 0 || args.length > 1)) {
-                sender.sendMessage(new TextComponentString(this.getUsage(sender)));
-                return;
-            }
-
-            level = parseInt(args[0]) - 1;
-            
-            if(level < -1 || level > 8){
-                sender.sendMessage(new TextComponentString("Level must be between 0 and 9"));
-                return;
-            }
-
-            sender.sendMessage(new TextComponentString("Setting Player to " + names[level+1]));
-
-            if (sender.getCommandSenderEntity() instanceof EntityPlayer) {
-                AllomancyCapabilities cap = AllomancyCapabilities.forPlayer(sender.getCommandSenderEntity());
-                cap.setAllomancyPower(level);
-                
-                Registry.network.sendTo(new AllomancyPowerPacket(level), (EntityPlayerMP) sender.getCommandSenderEntity());
-
+            if ((args.length == 0 || args.length > 2)) {
+                throw new WrongUsageException(this.getUsage(sender), new Object[0]);
             } else {
-                sender.sendMessage(new TextComponentString("Player not found"));
+
+                level = parseInt(args[0]) - 1;
+
+                EntityPlayer entityplayer = args.length > 1 ? getPlayer(server, sender, args[1]) : getCommandSenderAsPlayer(sender);
+
+                if (level < -1 || level > 8) {
+                    sender.sendMessage(new TextComponentString("Level must be between 0 and 9"));
+                    return;
+                }
+
+                if (entityplayer != null) {
+
+                    AllomancyCapabilities cap = AllomancyCapabilities.forPlayer(entityplayer);
+                    cap.setAllomancyPower(level);
+
+                    Registry.network.sendTo(new AllomancyPowerPacket(level), (EntityPlayerMP) entityplayer);
+
+                    notifyCommandListener(sender, this, "Setting " + entityplayer.getName() + " to " + names[level + 1], new Object[] { Integer.valueOf(level), entityplayer.getName() });
+
+                } else {
+                    sender.sendMessage(new TextComponentString("Player not found"));
+                }
             }
         }
     }
@@ -88,12 +89,12 @@ public class PowerCommand extends CommandBase {
 
     @Override
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos targetPos) {
-        return null;
+        return args.length == 2 ? getListOfStringsMatchingLastWord(args, server.getOnlinePlayerNames()) : Collections.<String>emptyList();
     }
 
     @Override
     public boolean isUsernameIndex(String[] args, int index) {
-        return false;
+        return index == 1;
     }
 
 }
