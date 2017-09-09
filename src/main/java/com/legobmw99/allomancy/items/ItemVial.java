148c3bbfec93bb8a7c22a5db170d529df3b59db9
Brian <legobmw99@gmail.com>
2017-09-08 20:25:57 -0400

    Add filled vials to creative menu

--- src/main/java/com/legobmw99/allomancy/items/ItemVial.java
+++ src/main/java/com/legobmw99/allomancy/items/ItemVial.java
@@ -9,15 +9,18 @@ import com.legobmw99.allomancy.util.AllomancyCapability;
 import com.legobmw99.allomancy.util.Registry;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
@@ -115,6 +118,22 @@ public class ItemVial extends Item {
     {
         return stack.hasTagCompound() ? EnumRarity.UNCOMMON : EnumRarity.COMMON;
     }
+    
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> subItems) {
+		if (isInCreativeTab(tab)) {
+			subItems.add(new ItemStack(this, 1, 0));
+			
+			ItemStack resultItem = new ItemStack(Registry.itemVial, 1);
+        	NBTTagCompound nbt = new NBTTagCompound();
+        	for(int i = 0; i < 8; i++){
+        		nbt.setBoolean(Registry.flakeMetals[i], true);
+        	}
+        	resultItem.setTagCompound(nbt);
+			subItems.add(resultItem);
+
+		}
+	}
     
  
 }
