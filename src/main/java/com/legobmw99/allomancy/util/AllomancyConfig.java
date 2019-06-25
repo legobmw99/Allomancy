ca0fa75594448bc19a5970fa8836eceaf6f1a75b
Brian <legobmw99@gmail.com>
2019-06-24 23:36:17 -0400

    More defaults

--- src/main/java/com/legobmw99/allomancy/util/AllomancyConfig.java
+++ src/main/java/com/legobmw99/allomancy/util/AllomancyConfig.java
@@ -32,7 +32,6 @@ public class AllomancyConfig {
     }
 
 
-
     public static boolean generate_copper;
     public static boolean generate_tin;
     public static boolean generate_lead;
@@ -97,11 +96,11 @@ public class AllomancyConfig {
             builder.pop();
             builder.push("Gameplay");
             random_mistings = builder.comment("Spawn players as a random Misting").define("random_mistings", true);
-            whitelist = builder.comment("List of registry names of items and blocks that are counted as 'metal").defineList("whitelist", default_whitelist(),o -> o instanceof String);
+            whitelist = builder.comment("List of registry names of items and blocks that are counted as 'metal").defineList("whitelist", default_whitelist(), o -> o instanceof String);
             builder.pop();
         }
 
-        private static List<String> default_whitelist(){
+        private static List<String> default_whitelist() {
             ArrayList<String> defaultList = new ArrayList<>();
             defaultList.add(Items.IRON_AXE.getRegistryName().toString());
             defaultList.add(Items.GOLDEN_AXE.getRegistryName().toString());
@@ -145,6 +144,11 @@ public class AllomancyConfig {
             defaultList.add(Items.GOLDEN_APPLE.getRegistryName().toString());
             defaultList.add(Items.GOLDEN_CARROT.getRegistryName().toString());
             defaultList.add(Items.IRON_SWORD.getRegistryName().toString());
+            defaultList.add(Items.IRON_NUGGET.getRegistryName().toString());
+            defaultList.add(Items.IRON_INGOT.getRegistryName().toString());
+            defaultList.add(Items.GOLD_NUGGET.getRegistryName().toString());
+            defaultList.add(Items.GOLD_INGOT.getRegistryName().toString());
+
             defaultList.add(Blocks.ANVIL.getRegistryName().toString());
             defaultList.add(Blocks.IRON_TRAPDOOR.getRegistryName().toString());
             defaultList.add(Blocks.IRON_DOOR.getRegistryName().toString());
@@ -163,6 +167,10 @@ public class AllomancyConfig {
             defaultList.add(Blocks.DETECTOR_RAIL.getRegistryName().toString());
             defaultList.add(Blocks.POWERED_RAIL.getRegistryName().toString());
             defaultList.add(Blocks.IRON_BLOCK.getRegistryName().toString());
+            defaultList.add(Blocks.IRON_ORE.getRegistryName().toString());
+            defaultList.add(Blocks.GOLD_BLOCK.getRegistryName().toString());
+            defaultList.add(Blocks.GOLD_ORE.getRegistryName().toString());
+
             defaultList.add("allomancy:vial");
             defaultList.add("allomancy:iron_lever");
             defaultList.add("allomancy:lerasium_nugget");
@@ -174,9 +182,8 @@ public class AllomancyConfig {
             }
 
             /*TagCollection<Item> tc = ItemTags.getCollection(); //get everything from item tags, in theory
-            Allomancy.LOGGER.debug("brian mark");
             Stream<ResourceLocation> rs =  tc.getRegisteredTags().stream().filter(p -> isMetalName(p.toString()));
-            rs.forEach(r -> tc.get(r).getAllElements().stream().map(i -> i.getRegistryName().toString()).forEach(System.out::println));*/
+            rs.forEach(r -> tc.get(r).getAllElements().stream().map(i -> i.getRegistryName().toString()).forEach(defaultList::add));*/
 
             defaultList.sort(String::compareTo);
 
@@ -218,22 +225,22 @@ public class AllomancyConfig {
         public final ForgeConfigSpec.BooleanValue animate_selection;
         public final ForgeConfigSpec.EnumValue<SCREEN_LOC> overlay_position;
 
-        ClientConfig(ForgeConfigSpec.Builder builder){
+        ClientConfig(ForgeConfigSpec.Builder builder) {
             builder.push("Graphics");
-            max_metal_detection = builder.comment("Maximum iron/steelsight distance").defineInRange("max_metal_distance",12,3,30);
+            max_metal_detection = builder.comment("Maximum iron/steelsight distance").defineInRange("max_metal_distance", 12, 3, 30);
             animate_selection = builder.comment("Animate the selection wheel").define("animate_selection", true);
             overlay_position = builder.comment("Screen Overlay Position").defineEnum("overlay_position", SCREEN_LOC.TOP_LEFT);
             builder.pop();
         }
     }
 
-    public static void refreshClient(){
+    public static void refreshClient() {
         max_metal_detection = CLIENT.max_metal_detection.get();
         animate_selection = CLIENT.animate_selection.get();
         overlay_position = CLIENT.overlay_position.get();
     }
 
-    public enum SCREEN_LOC{
+    public enum SCREEN_LOC {
         TOP_RIGHT,
         BOTTOM_RIGHT,
         TOP_LEFT,
