5bfcbb18f7641d305ba437f2025dd9bc733ad387
Brian <legobmw99@gmail.com>
2020-08-02 12:56:19 -0400

    Nerf Mending on Daggers

--- src/main/java/com/legobmw99/allomancy/modules/combat/item/ObsidianDaggerItem.java
+++ src/main/java/com/legobmw99/allomancy/modules/combat/item/ObsidianDaggerItem.java
@@ -2,10 +2,10 @@ package com.legobmw99.allomancy.modules.combat.item;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.IItemTier;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.*;
 import net.minecraft.item.crafting.Ingredient;
 
 public class ObsidianDaggerItem extends SwordItem {
@@ -49,8 +49,32 @@ public class ObsidianDaggerItem extends SwordItem {
         super(tier, ATTACK_DAMAGE, ATTACK_SPEED, new Item.Properties().group(ItemGroup.COMBAT));
     }
 
+    // prevent dagger from mining
     @Override
     public boolean canHarvestBlock(BlockState blockIn) {
         return false;
     }
+
+    // Disable mending on daggers
+    @Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        if (EnchantmentHelper.getEnchantments(book).containsKey(Enchantments.MENDING)) {
+            return false;
+        }
+        return super.isBookEnchantable(stack, book);
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        if (enchantment.equals(Enchantments.MENDING)) {
+            return false;
+        }
+        return super.canApplyAtEnchantingTable(stack, enchantment);
+    }
+
+    @Override
+    public float getXpRepairRatio(ItemStack stack) {
+        return 0;
+    }
+
 }
