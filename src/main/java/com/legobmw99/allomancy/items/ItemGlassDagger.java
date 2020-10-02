7c8896207a748952664f554282b15d830d6f5672
Ben <34463636+Benji123abc@users.noreply.github.com>
2020-10-02 17:12:32 -0400

    Fixed GlassDagger

--- src/main/java/com/legobmw99/allomancy/items/ItemGlassDagger.java
+++ src/main/java/com/legobmw99/allomancy/items/ItemGlassDagger.java
@@ -2,26 +2,15 @@ package com.legobmw99.allomancy.items;
 
 import com.legobmw99.allomancy.Allomancy;
 import com.legobmw99.allomancy.util.Registry;
-import net.minecraft.entity.Entity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemAxe;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.util.EnumHelper;
 
-public class ItemGlassDagger extends Item {
+public class ItemGlassDagger extends ItemAxe {
     public ItemGlassDagger() {
-        super();
+        super(EnumHelper.addToolMaterial("GLASS", 1, 200, 1, 1, 10), 1.5f, 0);
         this.setCreativeTab(Registry.tabsAllomancy);
-        this.maxStackSize = 1;
         this.setUnlocalizedName("glassdagger");
         this.setRegistryName(new ResourceLocation(Allomancy.MODID, "glassdagger"));
-        this.setContainerItem(this);
-    }
-
-    public float getDamageVsEntity(Entity entity) {
-        return 6;
-    }
-
-    public int getMaxItemUseDuration(ItemStack itemStack) {
-        return 3500;
     }
 }
