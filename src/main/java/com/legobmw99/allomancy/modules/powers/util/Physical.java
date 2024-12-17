e7a05883d862dddd14fb4c7253336efa86bcc5cd
Brian <legobmw99@gmail.com>
2024-12-17 17:39:30 -0500

    Add some missing metal items

--- src/main/java/com/legobmw99/allomancy/modules/powers/util/Physical.java
+++ src/main/java/com/legobmw99/allomancy/modules/powers/util/Physical.java
@@ -207,6 +207,9 @@ public final class Physical {
         add(Items.MUSIC_DISC_CREATOR_MUSIC_BOX);
         add(Items.TRIAL_KEY);
         add(Items.OMINOUS_TRIAL_KEY);
+        add(Items.MACE);
+        add(Items.SPYGLASS);
+        add(Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE);
 
         add(Blocks.ANVIL);
         add(Blocks.CHIPPED_ANVIL);
