c9655fccbdcb92d06a4aab54bd39234a6b74d2c0
Brian <legobmw99@gmail.com>
2025-01-02 21:35:46 -0500

    Move around

--- src/test/java/com/legobmw99/allomancy/test/modules/extras/BlocksTest.java
+++ src/test/java/com/legobmw99/allomancy/test/modules/extras/BlocksTest.java
@@ -1,4 +1,9 @@
 package com.legobmw99.allomancy.test.modules.extras;
 
+import net.neoforged.testframework.annotation.ForEachTest;
+
+@ForEachTest(groups = "blocks")
 public class BlocksTest {
+
+
 }
