b4441b3fffd6d45f4c0c107c31981cbd24a936eb
Brian <legobmw99@gmail.com>
2025-06-19 21:55:37 -0400

    Revert unnecessary name change

--- src/main/java/com/legobmw99/allomancy/modules/powers/network/AllomancerDataPayload.java
+++ src/main/java/com/legobmw99/allomancy/modules/powers/network/AllomancerDataPayload.java
@@ -19,8 +19,8 @@ public record AllomancerDataPayload(AllomancerData data, UUID player) implements
             StreamCodec.composite(AllomancerData.STREAM_CODEC, AllomancerDataPayload::data, UUIDUtil.STREAM_CODEC,
                                   AllomancerDataPayload::player, AllomancerDataPayload::new);
 
-    public static AllomancerDataPayload fromPlayer(ServerPlayer player) {
-        return new AllomancerDataPayload(player.getData(AllomancerAttachment.ALLOMANCY_DATA), player.getUUID());
+    public AllomancerDataPayload (ServerPlayer player) {
+        this(player.getData(AllomancerAttachment.ALLOMANCY_DATA), player.getUUID());
     }
 
     @Override
