214aabb940ae9fc6ab2e576fa943926b5f76722c
Jorphil <diverginglensestudios@gmail.com>
2026-05-06 20:39:58 +0200

    fixed line not rendering on some devices

--- src/main/java/com/legobmw99/allomancy/modules/powers/client/util/Rendering.java
+++ src/main/java/com/legobmw99/allomancy/modules/powers/client/util/Rendering.java
@@ -4,7 +4,9 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 
 public class Rendering {
@@ -18,7 +20,9 @@ public class Rendering {
      * @param dest
      * @param width  the width of the line
      */
-    public static void drawMetalLine(PoseStack stack,
+    /**Old version
+      public static void drawMetalLine(PoseStack stack,
+
                                      Vec3 player,
                                      Vec3 dest,
                                      float width,
@@ -41,9 +45,31 @@ public class Rendering {
 
         tessellator.end();
 
+    }*/
+
+    /** New version (Without width)
+     */
+    public static void drawMetalLine(PoseStack stack, Vec3 player, Vec3 dest, float width, float r, float g, float b) {
+        var bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+        VertexConsumer builder = bufferSource.getBuffer(RenderType.lines());
+        Matrix4f matrix4f = stack.last().pose();
+        Matrix3f normalMatrix = stack.last().normal();
+
+        builder.vertex(matrix4f, (float)player.x, (float)player.y, (float)player.z)
+                .color(r, g, b, 0.6F)
+                .normal(normalMatrix, 0, 1, 0)
+                .endVertex();
+        builder.vertex(matrix4f, (float)dest.x, (float)dest.y, (float)dest.z)
+                .color(r, g, b, 0.6F)
+                .normal(normalMatrix, 0, 1, 0)
+                .endVertex();
     }
 
     public static void doneDrawingLines(PoseStack stack) {
+
+        Minecraft.getInstance().renderBuffers().bufferSource().endBatch(RenderType.lines());
+
+        //old code (idk if it's still needed but it doesn't seem to break anything)
         stack.popPose();
         RenderSystem.applyModelViewMatrix();
 
